326fadc3462bee4280385d792528781276856d21
Luan Santos <luan.analista@me.com>
2025-06-19 23:02:46 -0300

    add route for order creation

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    Route::post('app/orders', [\App\Api\Controllers\OrderController::class, 'create']);    
+    Route::post('orders', [\App\Api\Controllers\OrderController::class, 'create']);    
 });
